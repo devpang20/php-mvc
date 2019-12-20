2f539a9f6b80e717639a20b92613bf7582620be8
팡식 <kwang20@plani.co.kr>
2019-12-20 10:52:15 +0900

    초기설정2

---
+++ application/libs/application.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace application\libs;
+
+class  Application
+{
+    public $controller;
+    public $action;
+
+    public function __construct()
+    {
+        $getUrl = '';
+        if (isset($_GET['url'])){
+            
+        }
+    }
+}
