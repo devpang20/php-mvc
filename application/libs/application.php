cfb04ed55d19b1d9515889ce4a7db74c825c50c4
sik2 <devpang20@gmail.com>
2019-12-30 10:10:47 +0900

    step 1

--- application/libs/application.php
+++ application/libs/application.php
@@ -11,7 +11,25 @@ class  Application
     {
         $getUrl = '';
         if (isset($_GET['url'])){
-            
+            $getUrl = rtrim($GET['url'], '/');
+            $getUrl = filter_var($get_Url, FILTER_SANITIZE_URL);
         }
+
+        $getParams = explode('/', getUrl);
+
+        $params['menu'] = isset($getParams[0]) && $getParams[0] != '' ? getParmas[0] : 'board';
+        $params['action'] = isset($getParams[1]) && $getParams[1] != '' ? $getParams[1] : 'index';
+        $params['category'] = isset($getParams[2]) && $getParams[2] != '' ? $getParams[2] : null;
+        $params['idx'] = isset($getParams[3]) && $getParams[3] != '' ? $getParams[3] : null;
+        $params['pageNum'] = isset($getParams[4]) && $getParmas[4] != '' ? $getParams[4] : null;
+    
+
+        if (!file_exists('application/controllers/'.$params['menu'].'Controller.php')) {
+            echo '해당 컨트롤러가 존재하지 않습니다.';
+            exit();
+        }
+
+        $controllerName = '\application\controllers\\'.$parmas['menu'].'controller';
+        new $controllerName($parmas['menu'], $params['action'], $params['category'], $params['idx'], $params['pageNum']);
     }
 }
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php 
-    echo 'test';
-?>
+
+require_once 'application/libs/config.php';
+require_once _ROOT.'/application/vender/autoload.php';
+
+new \application\libs\application();
